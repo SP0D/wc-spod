f2a433e01290b846a163d6fef271c48d47caee62
thomas-rothe <tho.rothe@googlemail.com>
2021-09-29 11:00:07 +0200

    remove tables.php and integrate in hooks
    tables.php is not necessary anymore, integrate table functions in activator and deactivator classes

--- classes/SpodPodActivator.php
+++ classes/SpodPodActivator.php
@@ -10,7 +10,39 @@ class SpodPodActivator {
 
 	public static function activate()
 	{
+        global $wpdb;
+
         flush_rewrite_rules();
+
+        $charset_collate = $wpdb->get_charset_collate();
+        $table_name = SPOD_SHOP_IMPORT_IMAGES;
+        $sql = "CREATE TABLE $table_name (
+          ID mediumint(9) NOT NULL AUTO_INCREMENT,
+          product_id int(128) NOT NULL,
+          images_data text,
+          action varchar(255) NOT NULL,
+          status int(11) NOT NULL,
+          attachment_id int(11),	
+          created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY  (id)
+        ) $charset_collate;";
+
+        $table_product = SPOD_SHOP_IMPORT_PRODUCTS;
+        $sql_product = "CREATE TABLE $table_product (
+          ID mediumint(9) NOT NULL AUTO_INCREMENT,
+          product_id int(128) NOT NULL,
+          title varchar(255) NOT NULL,
+          description text,
+          variants_data text,
+          images_data text,
+          status int(11) NOT NULL,
+          created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY  (id)
+        ) $charset_collate;";
+
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta($sql);
+        dbDelta($sql_product);
 	}
 	
 }
--- classes/SpodPodDeactivator.php
+++ classes/SpodPodDeactivator.php
@@ -15,11 +15,24 @@ class SpodPodDeactivator {
 	 */
 	public static function deactivate()
     {
+        global $wpdb;
+
         // plugin disconnection and product deleting only with woocommerce possible
         if ( function_exists( 'WC' ) ) {
             $Api = new SpodPodApiAuthentication();
             $Api->disconnectPlugin();
 	    }
+
+        // delete tables
+        $table_name = SPOD_SHOP_IMPORT_IMAGES;
+        $wpdb->query("TRUNCATE TABLE $table_name");
+        $wpdb->query("DROP TABLE $table_name");
+
+        $table_name = SPOD_SHOP_IMPORT_PRODUCTS;
+        $wpdb->query("TRUNCATE TABLE $table_name");
+        $wpdb->query("DROP TABLE $table_name");
+
+        flush_rewrite_rules();
 	}
 
 }
