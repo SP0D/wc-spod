3e84adf4a3bcb502c0c6b7cf6bcac613c3a6a505
thomas-rothe <tho.rothe@googlemail.com>
2022-11-24 18:06:12 +0100

    remove old code fragment

--- classes/SpodPodApiHandler.php
+++ classes/SpodPodApiHandler.php
@@ -11,7 +11,6 @@
 class SpodPodApiHandler
 {
     protected $api_url = 'https://rest.spod.com/';
-    protected $rest_api_url = 'https://ff.spod-staging.com/fulfillment/woo-commerce/oauth/initiate?url=';
 
     /**
      * get main curl options
@@ -27,20 +26,6 @@ class SpodPodApiHandler
              'X-SPOD-ACCESS-TOKEN' => get_option('ng_spod_pod_token')
          ];
      }
-
-    /**
-     * get rest api curl options
-     * @since      1.1.0
-     * @return array $options
-     */
-    protected function getRestApiOptions()
-    {
-        return [
-            'accept' => 'application/json',
-            'content-Type' => 'application/json',
-            'user-agent' => 'WooCommerce/'.SPOD_POD_VERSION,
-        ];
-    }
 
     /**
      * set request via curl
